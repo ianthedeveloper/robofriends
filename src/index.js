681d226320e748beac244eaa8a5640947baa6c11
mungaii <theforexer1@gmail.com>
2024-04-04 12:23:22 +0300

    Modifying create-react-app to default status

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Hello from './Hello';
+import './index.css';
+import 'tachyons';
+
+ReactDOM.render(
+  <Card/>,
+  document.getElementById('root')
+);
