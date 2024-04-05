0962cb3a50bf8e2f74d0797083ce6294c2e24c56
mungaii <theforexer1@gmail.com>
2024-04-05 16:14:39 +0300

    Enabling communication between the SearchBox Component & the CardList Component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,40 @@ import SearchBox from './SearchBox';
 import CardList from './CardList';
 import {robots} from './robots';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: robots,
-            searchfield: ' '
+class App extends Component 
+{
+        constructor() {
+            super()
+            this.state = {
+                robots: robots,
+                searchfield: ''
+            }
         }
-    }
 
-    onSearchChange = (event) => {
-        console.log(event.target.value);
-    }
+        // toLowerCase is used below to conver the specified data to Lowercase format
 
-    render () {
-        return (
-            <div className='tc'>
-                <h2>RobotFriends</h2>
-                <SearchBox searchChange = {this.onSearchChange}/>
-                <CardList robots = {this.state.robots}/>
-            </div>
-        );
-    }
+        onSearchChange = (event) => {
+            this.setState({searchfield: event.target.value});
+
+        }
+
+
+
+        render () {
+
+            const filteredRobots = this.state.robots.filter((robot) => {
+                return 
+                robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+            })
+
+            return (
+                <div className='tc'>
+                    <h2>RobotFriends</h2>
+                    <SearchBox searchChange = {this.onSearchChange}/>
+                    <CardList robots = {filteredRobots}/>
+                </div>
+            );
+        }
 }
 
 export default App;
