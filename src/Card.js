018c999656b964cb8353eeb62af156fe7420ee02
mungaii <theforexer1@gmail.com>
2024-04-07 14:27:41 +0300

    Writing the Card Component in a different style

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,47 @@
+// import React from 'react';
+
+// const Card = ({id, name, email}) => {
+//     return (
+//         <div className='f6 tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-8'>
+
+//             <img src={`https://robohash.org/${id}?212%212" alt="robot img`}/>
+//             <div>
+//                 <h1> {name}  </h1>
+//                 <p>  {email} </p>
+//             </div>
+
+//         </div>
+//     );
+// }
+
+// export default Card;
+
+
+
+
+
+
+
+
+
+
+// const {id, name, email} = props;
+
 import React from 'react';
 
-const Card = ({id, name, email}) => {
+const Card = () => {
     return (
+        // const {id, name, email} = props;
+
         <div className='f6 tc bg-light-red dib br3 pa3 ma2 grow bw2 shadow-8'>
-            <img src={`https://robohash.org/${id}?212%212" alt="robot img`}/>
-            <div>
-                <h1> {name}  </h1>
-                <p>  {email} </p>
-            </div>
+
+            const {id, name, email} = props;
+
+            <img src = "https://robohash.org/textintextin"/>
+            <p>{id}</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
+
         </div>
     );
 }
