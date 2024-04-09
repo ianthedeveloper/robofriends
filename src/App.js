ea6b6c28a1a73f48a1904352ad0f75619728336f
mungaii <theforexer1@gmail.com>
2024-04-09 14:44:32 +0300

    Implemented APIs to get users

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
-import {robots} from './robots';
+// import {robots} from './robots';
 import './App.css';
 
 class App extends Component {
         constructor() {
             super()
             this.state = {
-                robots: robots,
+                robots: [],
                 searchfield: ''
             }
         }
@@ -21,10 +21,16 @@ class App extends Component {
 
         }
 
+        componentDidMount(){
+            fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState({robots:users}))
+        }
+
 
         render () {
 
-            const filteredRobots = this.state.robots.filter(robots => {
+            const filteredRobots = this.state.robots.filter((robots) => {
                 return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
             });
 
