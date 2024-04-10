d2902f650f47c5e9aae651db05cb83def8f9f23d
mungaii <theforexer1@gmail.com>
2024-04-10 19:51:21 +0300

    Using useState & useEffect React Hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
@@ -6,60 +6,50 @@ import ErrorBoundary from '../components/ErrorBoundary';
 // import {robots} from './robots';
 import './App.css';
 
-class App extends Component {
-        constructor() {
-            super()
-            this.state = {
-                robots: [],
-                searchfield: ''
-            }
-        }
+
+// Using React Hooks
+function App () {
+
+        // Introducing STATE
+        const [robots, setRobots] = useState([]);
+        const [searchfield, setSearchfield] = useState(' ');
 
         // toLowerCase is used below to convert the specified data to Lowercase format
-
-        onSearchChange = (event) => {
-            this.setState({searchfield: event.target.value});
-        }
+        const onSearchChange = (event) => {setSearchfield(event.target.value)}
 
         // Using APIs to get users from somewhere else on the web
-
-        componentDidMount(){
+        useEffect(() => {
             fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
-            .then((users) => {this.setState({robots: users})});
-        }
+            .then((users) => {setRobots(users)})
+        })
 
-
-        render () {
-            // To avoid repition of 'this.state'
-            const {robots, searchfield} = this.state;
-
-            const filteredRobots = robots.filter((robots) => {
-                return robots.name.toLowerCase().includes(searchfield.toLowerCase())
+        const filteredRobots = robots.filter((robots) => {
+            return robots.name.toLowerCase().includes   (searchfield.toLowerCase())
             });
 
-            // Implementing a LOADING: alert in the event the webpage takes too long to load
+        // Implementing a LOADING: alert in the event the webpage takes too long to load
 
-            if (robots.length === 0){
-                return <h2>LOADING: </h2>
+        if (robots.length === 0){
+            return <h2>LOADING: </h2>
             } else {
-                return (
+                    return (
 
-                    <div className='tc'>
-                        <h2>RobotFriends</h2>
-                        <SearchBox searchChange = {this.onSearchChange}/>
-                        <Scroll>
-                            <ErrorBoundary>
-                                <CardList robots = {filteredRobots}/>
-                            </ErrorBoundary>
-                        </Scroll>
-                        
-                    </div>
-                );
-            }
-        }
+                        <div className='tc'>
+                                <h2>RobotFriends</h2>
+                                <SearchBox searchChange = {onSearchChange}/>
+                                <Scroll>
+                                    <ErrorBoundary>
+                                        <CardList robots = {filteredRobots}/>
+                                    </ErrorBoundary>
+                                </Scroll>
+                                    
+                            </div>
+                            );
+                    }
+    }
 
-}
+
 
 export default App;
 
